432f412246831e52047503177ee303c5704db7ab
Martin Pinter <mpinter.pinto@gmail.com>
2015-09-15 12:00:47 +0200

    prefaps

---
+++ FromLight/Assets/Resources/Scripts/GenerateScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateScript : MonoBehaviour {
+
+    public GameObject lastCheckpoint;
+    public GameObject currentCheckpoint;
+
+	void Start () {
+	    //generate first
+
+	}
+	
+	void Update () {
+	
+	}
+
+    public void generateNext()
+    {
+
+    }
+}
