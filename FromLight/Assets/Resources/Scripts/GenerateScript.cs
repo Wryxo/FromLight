530898fd5f1eb15c921602664b88a54735fe30df
Martin Pinter <mpinter.pinto@gmail.com>
2015-09-15 17:27:10 +0200

    wat i dont even

--- FromLight/Assets/Resources/Scripts/GenerateScript.cs
+++ FromLight/Assets/Resources/Scripts/GenerateScript.cs
@@ -11,6 +11,7 @@ public class GenerateScript : MonoBehaviour {
 
     public List<GameObject> blocks = new List<GameObject>();
     public List<GameObject> safeSpots = new List<GameObject>();
+    public List<List<GameObject>> stageLists = new List<List<GameObject>>();
 
     void Awake()
     {
@@ -24,29 +25,41 @@ public class GenerateScript : MonoBehaviour {
 	}
 	
 	void Update () {
-	
-	}
+        if (Input.GetKeyDown("space"))
+        {
+            generateNextPoint();
+        }
+    }
 
     public void generateNextPoint() {
+        //horizontalDirection == true => came from right, else came from left
+        bool horizontalDirection = lastCheckpoint.transform.position.x > currentCheckpoint.transform.position.x;
         if (lastCheckpoint != null) Destroy(lastCheckpoint);
         lastCheckpoint = currentCheckpoint;
+        Vector2 lastPoint = lastCheckpoint.transform.position;
         // TODO: decide between building blocks and simple transition
         if (true)
         {
             //simple
-            Vector2 point = Random.insideUnitCircle*test_distance;
-            point.x = point.x > 0 ? point.x + test_distance : point.x - test_distance;
-            currentCheckpoint = Instantiate(Resources.Load( "Prefabs/Generator/Checkpoint", typeof(GameObject))) as GameObject;
-            currentCheckpoint.transform.position = point;
-            GameObject safeSpot = Instantiate(Resources.Load("Prefabs/Generator/Safespot", typeof(GameObject))) as GameObject;
-            point.y = point.y - 5;
-            safeSpot.transform.position = point;
-            safeSpots.Add(safeSpot);
+            Vector2 point = new Vector2(Random.Range(20f,40f), Random.RandomRange(3f,30f));
+            if (Random.value > 0.5f) point.x = -1f * point.x;
+            generateIsland(point);
             // TODO: assign blocks
         } else
         {
-            //blocks
-            
+            // TODO make this less random
+            int limit = Random.RandomRange(1,3);
+            for (int i=0;i< limit;i++)
+            {
+                //stages - always generate towards the direction opposite to the one player just came from
+                GameObject stage = horizontalDirection ? getLeftStage() : getRightStage();
+                //stage origin is also it's entry point
+                stage.transform.position = lastPoint;
+                lastPoint = stage.transform.position;
+                //resources
+                stage.GetComponent<StageScript>
+            }
+            generateIsland(lastPoint);
         }
     }
 
@@ -54,4 +67,28 @@ public class GenerateScript : MonoBehaviour {
     {
 
     }
+
+    // TODO: get random
+    private GameObject getLeftStage()
+    {
+        return Instantiate(Resources.Load("Prefabs/Generator/Stages/", typeof(GameObject))) as GameObject;
+    }
+
+    // TODO: get random
+    private GameObject getRightStage()
+    {
+        return Instantiate(Resources.Load("Prefabs/Generator/Stages/", typeof(GameObject))) as GameObject;
+    }
+
+    private void generateIsland(Vector2 point)
+    {
+        currentCheckpoint = Instantiate(Resources.Load("Prefabs/Generator/Checkpoint", typeof(GameObject))) as GameObject;
+        currentCheckpoint.transform.position = point;
+        GameObject safeSpot = Instantiate(Resources.Load("Prefabs/Blocks/SafespotPlacehold", typeof(GameObject))) as GameObject;
+        point.y = point.y - 1.2f;
+        point.x = point.x - 1.2f;
+        safeSpot.transform.position = point;
+        safeSpots.Add(safeSpot);
+    }
+
 }
