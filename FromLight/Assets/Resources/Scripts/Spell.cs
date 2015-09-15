888e07861a76d703c66598aa161573d5d9f51020
Martin Strapko <martin.strapko@microstep-hdo.sk>
2015-09-15 17:28:40 +0200

    Pridany Spell

---
+++ FromLight/Assets/Resources/Scripts/Spell.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spell {
+	public Dictionary<string, int> Projectile = new Dictionary<string, int>();
+	public Block Blok;
+	public int ManaCost;
+}
