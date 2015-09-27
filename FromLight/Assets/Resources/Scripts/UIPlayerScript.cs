f4ab8a8f97b547aa2dd89028c5fcc7a0e7959d8f
Martin Pinter <mpinter.pinto@gmail.com>
2015-09-27 11:30:56 +0200

    mana bar

---
+++ FromLight/Assets/Resources/Scripts/UIPlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIPlayerScript : MonoBehaviour {
+
+    public GameObject ManaBar;
+
+	// Use this for initialization
+	void Start () {
+        ManaBar.GetComponentInChildren<Image>().fillMethod = Image.FillMethod.Radial360;
+        ManaBar.GetComponentInChildren<Image>().type = Image.Type.Filled;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ManaBar.GetComponentInChildren<Image>().fillAmount = (float)gameObject.GetComponent<PlayerScript>().Mana / (float)gameObject.GetComponent<PlayerScript>().ManaCap;
+    }
+}
