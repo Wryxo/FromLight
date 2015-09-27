9d45caf8876b575d5fb316adac965e3289c506ca
orcist <motyl.kubo@gmail.com>
2015-09-27 16:04:54 +0200

    Various projectile colors depending on bounce number

--- FromLight/Assets/Resources/Scripts/ProjectileScript.cs
+++ FromLight/Assets/Resources/Scripts/ProjectileScript.cs
@@ -3,7 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ProjectileScript : MonoBehaviour {
+    public GameObject BlockObject;
+    public GameObject RedPS, GreenPS, BluePS;
     private Dictionary<string, int> config;
+    
     private BlockScript blok;
     private Rigidbody2D rb;
 
@@ -16,8 +19,27 @@ public class ProjectileScript : MonoBehaviour {
             GetComponent<Collider2D>().isTrigger = true;
         if (config["gravity"] == 0)
             rb.gravityScale = 0;
+
+        switch (config["bounces"]) {
+            case 0:
+                setColorGradient(RedPS);
+                break;
+            case 1:
+                setColorGradient(GreenPS);
+                break;
+            default:
+                setColorGradient(BluePS);
+                break;
+        }
+    }
+    private void setColorGradient(GameObject ps) {
+        GameObject gops = (GameObject)GameObject.Instantiate(ps, Vector3.zero, Quaternion.identity);
+        gops.transform.SetParent(gameObject.transform, false);
+        gops.GetComponent<ParticleSystem>().Play();
     }
     public void resolve() {
+        GameObject b = (GameObject)GameObject.Instantiate(BlockObject, transform.position, Quaternion.identity);
+        b.AddComponent(blok.GetType());
         Destroy(gameObject);
     }
     void OnCollisionEnter2D(Collision2D col) {
@@ -30,7 +52,6 @@ public class ProjectileScript : MonoBehaviour {
 	public int getOnFire(){
 		return config["onFire"];
 	}
-
     void FixedUpdate() {
         rb.velocity *= (1f - ((float)config["slowDown"]) / 100f);
 		if (Mathf.Abs(rb.velocity.x) < 0.2f && rb.velocity.magnitude < 5f && config["slowDown"] != 0) {
